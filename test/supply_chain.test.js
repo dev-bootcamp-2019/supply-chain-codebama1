031bd72c3d7e229a6bebc77feb852a3d945eaabe
Bama Venkatachalam <bama.venkatachalam@gtnexus.com>
2019-01-09 01:42:07 -0800

    Big number addition fix in js

--- test/supply_chain.test.js
+++ test/supply_chain.test.js
@@ -46,7 +46,7 @@ contract('SupplyChain', function(accounts) {
         const supplyChain = await SupplyChain.deployed()
 
         var eventEmitted = false
-        const amount = "2000" 
+        var amount = "1000" 
 
         var aliceBalanceBefore = await web3.eth.getBalance(alice)
         var bobBalanceBefore = await web3.eth.getBalance(bob)
@@ -59,6 +59,8 @@ contract('SupplyChain', function(accounts) {
 	}
 
         var aliceBalanceAfter = await web3.eth.getBalance(alice)
+        var a = add(aliceBalanceBefore, amount)
+        console.log(a)
         var bobBalanceAfter = await web3.eth.getBalance(bob)
 
         const result = await supplyChain.fetchItem.call(sku)
@@ -66,7 +68,7 @@ contract('SupplyChain', function(accounts) {
         assert.equal(result[3].toString(10), 1, 'the state of the item should be "Sold", which should be declared second in the State Enum')
         assert.equal(result[5], bob, 'the buyer address should be set bob when he purchases an item')
         assert.equal(eventEmitted, true, 'adding an item should emit a Sold event')
-        assert.equal(parseInt(aliceBalanceAfter), parseInt(aliceBalanceBefore, 10) + parseInt(price, 10), "alice's balance should be increased by the price of the item")
+        assert.equal(parseInt(aliceBalanceAfter), add(aliceBalanceBefore , price), "alice's balance should be increased by the price of the item")
         assert.isBelow(parseInt(bobBalanceAfter), parseInt(bobBalanceBefore, 10) - parseInt(price, 10), "bob's balance should be reduced by more than the price of the item (including gas costs)")
     })
 
@@ -107,3 +109,39 @@ contract('SupplyChain', function(accounts) {
     })
 
 });
+//Big number addition when it exceeds max allowed int value in js
+function add(str1, str2) {
+
+    let sum = "";  // our result will be stored in a string.
+
+    // we'll need these in the program many times.
+    let str1Length = str1.length;
+    let str2Length = str2.length;
+
+    // if s2 is longer than s1, swap them.
+    if(str2Length > str1Length ){
+        let temp = str2;
+        str2 = str1;
+        str1 = temp;
+    }
+
+    let carry = 0;  // number that is carried to next decimal place, initially zero.
+    let a;
+    let b;
+    let temp;
+    let digitSum;
+    for (let i = 0; i < str1.length; i++) {
+        a = parseInt(str1.charAt(str1.length - 1 - i));      // get ith digit of str1 from right, we store it in a
+        b = parseInt(str2.charAt(str2.length - 1 - i));      // get ith digit of str2 from right, we store it in b
+        b = (b) ? b : 0;                                    // make sure b is a number, (this is useful in case, str2 is shorter than str1
+        temp = (carry + a + b).toString();                  // add a and b along with carry, store it in a temp string.
+        digitSum = temp.charAt(temp.length - 1);            //
+        carry = parseInt(temp.substr(0, temp.length - 1));  // split the string into carry and digitSum ( least significant digit of abSum.
+        carry = (carry) ? carry : 0;                        // if carry is not number, make it zero.
+
+        sum = (i === str1.length - 1) ? temp + sum : digitSum + sum;  // append digitSum to 'sum'. If we reach leftmost digit, append abSum which includes carry too.
+
+    }
+    return sum;     // return sum
+
+}
